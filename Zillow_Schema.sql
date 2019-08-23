bf7d66d260581d441d4e52b110e48c32a26e9439
meharperjr <meharperjr@gmail.com>
2019-08-23 16:03:57 -0700

    starting zilliow scrape

---
+++ Zillow_Schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE property(
+	id INT PRIMARY KEY,
+	Title TEXT,
+	Address TEXT,
+	City TEXT,
+	State TEXT,
+	Zip_Code INT,
+	Price FLOAT,
+	Facts_AND_Features TEXT,
+	Real_Estate_Provider TEXT,
+	URL TEXT	
+);
