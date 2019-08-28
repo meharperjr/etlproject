0baad3f990d1cbf71dded0cd7b4b76b27cafa580
Jessica Lai <jessicaklai@gmail.com>
2019-08-27 21:14:52 -0700

    schema-updated

--- resources/Zillow_Schema.sql
+++ resources/Zillow_Schema.sql
@@ -11,12 +11,22 @@ CREATE TABLE property(
 
 SELECT * FROM property;
 
-CREATE TABLE property_april(
-	id INT PRIMARY KEY,
-	Zip_code TEXT,
-	Date	TEXT,
-	median_price FLOAT,
-	Sale_count	FLOAT
-);
+DROP TABLE IF EXISTS beaverton_merged;
 
-SELECT * FROM property_april;
+CREATE TABLE beaverton_merged(
+	Month TEXT,
+	ZipCode TEXT PRIMARY KEY,
+	ZipName TEXT,
+	Nielsen_HH_Rank FLOAT,
+	Hotness_Rank_County FLOAT,
+	Hotness_Rank_MoM FLOAT,
+	Hotness_Rank_YoY FLOAT,
+	Hotness_Score FLOAT,
+	Supply_Score FLOAT,
+	Demand_Score FLOAT,
+	Date TEXT,
+	median_price FLOAT,
+	Sale_count FLOAT
+	);
+	
+SELECT * FROM beaverton_merged;
